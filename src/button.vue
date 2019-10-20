5f07785b0f023a4238d06be739c0985dd8fd0267
NorthwesternDirector <fanshundee@126.com>
2019-10-20 20:42:07 +0800

    创建 button-group，解决边框重叠问题

--- src/button.vue
+++ src/button.vue
@@ -11,7 +11,6 @@
 
 <script>
     export default {
-        // props:['iconName','iconPosition']
         props:{
             iconName:{},
             iconPosition:{
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('nd-button', Button)
 Vue.component('nd-icon', Icon)
+Vue.component('nd-button-group', ButtonGroup)
 
 new Vue({
     el: "#app",
