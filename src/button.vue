a88f78437e281a3d8a1a84e58e1aa2dd68be18c7
NorthwesternDirector <fanshundee@126.com>
2019-10-20 17:09:11 +0800

    设置iconName iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,19 @@
 <template>
-  <button class="nd-button">嗨～</button>
+
+    <button class="nd-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="iconName" class="icon">
+            <use :xlink:href="`#i-${iconName}`"></use>
+        </svg> 
+        <div class="icon-content">
+            <slot></slot>
+        </div>
+    </button>
+  
 </template>
 
 <script>
     export default {
-
+        props:['iconName','iconPosition']
     }
 </script>
 
@@ -12,21 +21,40 @@
     .nd-button{
         font-size: var(--font-size);
         height: var(--button-height);
-        padding: 0 1em;
+        padding: 0 .8em;
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display:inline-flex;
+        justify-content: center;
+        align-items:center;
+        vertical-align: middle;
 
         &:hover {
             border-color: var(--border-color-hover);
         }
-        
         &:active {
             background-color: var(--button-active-bg);
         }
-        
         &:focus {
             outline: none;
+        }
+        > .icon{
+            order:1;
+            margin-right: .3em;
+        }
+        > .icon-content{
+            order:2;
+        }
+        &.icon-right{
+            > .icon-content{
+                order:1;
+            }
+            > .icon{
+                order:2;
+                margin-right: 0;
+                margin-left: .3em;
+            }
         }
     }
 </style>
